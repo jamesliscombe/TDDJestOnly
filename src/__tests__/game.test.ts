aed8e2475bc160cf66f526dc4999cba59414b15a
James Liscombe <jliscombe@me.com>
2024-07-14 09:20:19 +0100

    chore: singleton pattern

--- src/__tests__/game.test.ts
+++ src/__tests__/game.test.ts
@@ -1,15 +1,48 @@
-import {welcomeUser} from '../game';
+import {Game} from '../game';
 
 describe('Game', () => {
+  let originalPrompt: typeof global.prompt;
+  let game: Game;
+
+  afterAll(() => {
+    global.prompt = originalPrompt;
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  beforeAll(() => {
+    originalPrompt = global.prompt;
+  });
+
+  beforeEach(() => {
+    game = Game.getInstance();
+  });
+
   it('should log the users name entered in the welcome prompt', () => {
     const mockPrompt = jest.fn().mockReturnValue('James Liscombe');
 
     (global as unknown as {prompt: () => string}).prompt = mockPrompt;
 
-    const result = welcomeUser();
+    const result = game.welcomeUser();
 
     expect(result).toBe('Hello James Liscombe');
 
     mockPrompt.mockRestore();
+  });
+
+  it('should return the users name if it has already been entered', () => {
+    game.setUserName('James Liscombe');
+
+    const result = game.welcomeUser();
+
+    expect(result).toBe('Hello James Liscombe, welcome back');
+  });
+
+  it('should return the users name', () => {
+    game.setUserName('James Liscombe');
+
+    expect(game.getUserName()).toBe('James Liscombe');
   });
 });
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,33 @@
-const welcomeUser = () => {
-  const helloPrompt = prompt('what is your name?');
-  return `Hello ${helloPrompt}`;
-};
+class Game {
+  private static instance: Game;
+  private userName: string | null = '';
 
-export {welcomeUser};
+  private constructor() {}
+
+  public static getInstance(): Game {
+    if (!Game.instance) {
+      Game.instance = new Game();
+    }
+    return Game.instance;
+  }
+
+  public welcomeUser(): string {
+    if (this.userName) {
+      return `Hello ${this.userName}, welcome back`;
+    }
+
+    this.userName = prompt('Please enter your name');
+
+    return `Hello ${this.userName}`;
+  }
+
+  public getUserName(): string | null {
+    return this.userName;
+  }
+
+  public setUserName(name: string): void {
+    this.userName = name;
+  }
+}
+
+export {Game};
