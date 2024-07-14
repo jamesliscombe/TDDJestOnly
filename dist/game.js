084ebeb2e854840384ee8a8d5855e8ac9e368e79
James Liscombe <jliscombe@me.com>
2024-07-14 08:52:31 +0100

    fix: removing node_modules from git

--- dist/game.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.welcomeUser = void 0;
-const welcomeUser = () => {
-    const helloPrompt = prompt('what is your name?');
-    return `Hello ${helloPrompt}`;
-};
-exports.welcomeUser = welcomeUser;
